2fcc60fd168e4eaf9f64daecf8d607cf6e36c402
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-05-12 19:36:23 -0400

    Precip Data into Flask

--- app.py
+++ app.py
@@ -34,9 +34,19 @@ def welcome():
     return(
     '''
     Welcome to the Climate Analysis API!
-    Available Routes:\n
-    /api/v1.0/precipitation\n
-    /api/v1.0/stations\n
-    /api/v1.0/tobs\n
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+   
