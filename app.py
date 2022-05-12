264eb08f80ae874d2305f5fcdae9c1fbe5dc9550
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-05-12 19:28:44 -0400

    Added to Flask App

--- app.py
+++ app.py
@@ -7,9 +7,36 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# app = Flask(__name__)
+
+# @app.route('/')
+
+# # def hello_world():
+# #     return 'Hello World'
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
+
+session = Session(engine)
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 
-def hello_world():
-    return 'Hello World'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
